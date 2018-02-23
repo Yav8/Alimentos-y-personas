939e803af5d2d5469d78edeae5352daf01214e36
Yav <yavieron133@gmail.com>
2018-02-23 12:52:43 +0100

    Modificado método constestar que proporciona respuestas a la pregunta introducida por el usuario relacionadas con la cantidad de calorías ingeridas por la persona

--- Persona.java
+++ Persona.java
@@ -9,7 +9,8 @@ public class Persona {
     private int centimetros;
     private int anos;
     private int totalCalorias;
-    
+    private boolean haSobrepasadoElLimite;
+
     /**
      * Constructor de la clase Persona.
      * @param nombre El nombre de la persona.
@@ -25,8 +26,9 @@ public class Persona {
         this.centimetros = centimetros;
         this.anos = anos;
         totalCalorias = 0;
+        haSobrepasadoElLimite = false;
     }
-    
+
     /**
      * Permite a la persona comer.
      * @param comida Objeto comida que comerá la persona.
@@ -44,7 +46,7 @@ public class Persona {
         }
         return caloriasDeLaComida;
     }
-    
+
     /**
      * Permite conocer la cantidad de calorías que ha ingerido
      * una persona.
@@ -53,5 +55,30 @@ public class Persona {
      */
     public int getCaloriasIngeridas() {
         return totalCalorias;
+    }
+
+    /**
+     * Contesta a la pregunta realizada por el usuario sobre cuestiones
+     * relacionadas con la cantidad de calorías ingeridas por las personas. 
+     * @param pregunta La pregunta introducida por el usuario.
+     * @return pregunta La respuesta a la pregunta.
+     */
+    public String contestar(String pregunta) {
+        if((hombre == true && totalCalorias > ((10 * kilogramos) + (6 * centimetros) + (5 * anos) + 5)) || (hombre == false && totalCalorias > ((10 * kilogramos) + (6 * centimetros) + (5 * anos) - 161))) {
+            haSobrepasadoElLimite = true;
+        }
+        if(haSobrepasadoElLimite == true || pregunta.contains(nombre)) {
+            pregunta = pregunta.toUpperCase();
+        }
+        else {
+            if(pregunta.length() % 3 == 0) {
+                pregunta = "SI";
+            }
+            else if(pregunta.length() % 3 != 0) {
+                pregunta = "NO";
+            }
+        }
+        System.out.println(pregunta);
+        return pregunta;
     }
 }
